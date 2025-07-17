1e79a866002b9d15c7d53a44584da4cda2138cf9
Hamza <sahamzashah19@gmail.com>
2025-07-17 18:10:28 +0500

    fix: update greeting message in FastAPI to correct punctuation

--- app/main.py
+++ app/main.py
@@ -4,4 +4,4 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"message": "Hello Hamza, Docker + FastAPI! Running!"}
+    return {"message": "Hello Hamza!, Docker + FastAPI! Running!"}
